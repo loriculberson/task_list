6ee5e7587f23a58069b66ff2a471f48d07440b7d
Lori Culberson <bigskybandit@sbcglobal.net>
2015-05-09 12:05:42 -0600

    Add new user form Active nav bar links to sign up page Add css for nav bar

--- spec/unauthorized_user/new_user_account_creation_spec.rb
+++ spec/unauthorized_user/new_user_account_creation_spec.rb
@@ -2,9 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "new user can create account", type: :feature do  
 
+
   it "sees the sign up link on the homepage" do
-    visit root_path
+    visit '/'
     expect(page).to have_content("Sign Up")
+  end
+
+  it "can see form for registration" do
+    visit '/'
+    click_link "Sign Up"
+
+    expect(page).to have_button("Create Account")
+    expect(page).to have_content("Username")
+    expect(page).to have_content("Password")
+
+    expect(page).not_to have_content("first_name")
+  end
+
+  it "can create an account" do
+    visit '/'
+    click_link "Sign Up"
+    fill_in("user[username]", with: "buttercup" )
+    fill_in("user[display_name]", with: "rockstar" )
+    fill_in("user[email]", with: "happy@example.com" )
+    fill_in("user[password]", with: "password" )
+    fill_in("user[password_confirmation]", with: "password" )
+    click_button "Create Account"
+
+    expect(page).to have_content("Account created!")
+    expect(page).to have_content("Task Lists")
   end
 
 
