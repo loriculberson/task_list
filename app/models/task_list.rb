4978df0bd1f2efc94ddfbb9e780f7b8c9ed70c66
Lori Culberson <bigskybandit@sbcglobal.net>
2015-05-10 15:08:31 -0600

    Validate task_list attributes in model spec Remove custom flash message from create action if task list could not be saved Add standard task error messages

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,6 +1,5 @@
 class TaskList < ActiveRecord::Base
   validates :title, :user_id, presence: true
-  validates :title, uniqueness: true
   belongs_to :user
 end
 
--- spec/models/task_list_spec.rb
+++ spec/models/task_list_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe TaskList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "task_list with valid attributes" do
+
+    it "is valid with all attributes" do
+      list = create(:task_list)
+      expect(list).to be_valid
+    end
+   
+    it "is valid with a false value for archived status" do
+      list = TaskList.create(title: "Swim lessons", archived: false, user_id: 2)
+      expect(list).to be_valid
+    end
+
+    it "is valid with a true value for archived status" do
+      list = TaskList.create(title: "Swim lessons", archived: true, user_id: 2)
+      expect(list).to be_valid
+    end
+
+    it "is valid with a default value for archived status" do
+      list = TaskList.create(title: "Swim lessons", user_id: 2)
+      expect(list).to be_valid
+    end
+
+  end
+
+  context "task_list without valid attributes" do
+
+    it "is not valid without a title" do
+      list = TaskList.create(title: nil)
+      expect(list).not_to be_valid
+    end
+
+    it "is not valid without a user id" do
+      list = TaskList.create(title: "Swim lessons", archived: true, user_id: nil)
+      expect(list).not_to be_valid
+    end
+  end
 end
