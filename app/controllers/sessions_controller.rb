d20fe30b777d053e32f7eaaa4752e89120ad3a57
Lori Culberson <bigskybandit@sbcglobal.net>
2015-05-09 18:29:58 -0600

    Add sessions create so user can be authenticated and login Modified navbar to accommodate login User gets errors if signup is invalid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in!"
+      redirect_to task_lists_path
+    else
+      flash[:errors] = "Invalid login. Please try again."
+      redirect_to :back
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root "home#index"
+  post '/login'=> 'sessions#create'
 
   resources :users
   resources :task_lists
